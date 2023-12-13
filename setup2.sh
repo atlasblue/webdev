38477fea11f42a63d390a1cb7a1b8d9901551995
atlasblue <92083755+atlasblue@users.noreply.github.com>
2023-12-13 01:11:25 +0100

    Update setup2.sh

--- setup2.sh
+++ setup2.sh
@@ -50,13 +50,15 @@ echo "
 <p>&nbsp;</p>
 <p><span style="font-size:36px"><span style="font-family:Arial,Helvetica,sans-serif"><strong>Welcome to the Development environment</strong></span></span></p>
 <p>&nbsp;</p>
-<?php
+<?php 
 $ip_server = $_SERVER['SERVER_ADDR']; 
 echo '<span style="font-size:36px"><span style="font-family:Arial,Helvetica,sans-serif"><strong>';
-echo "Server IP Address is : $ip_server"; 
+echo 'Server IP Address is : $ip_server'; 
 ?> 
 <p><img src="dev.png" /></p>
 " > $serverRoot/$router
+
+
 wget https://raw.githubusercontent.com/atlasblue/webdev/master/dev.png -P $serverRoot
 chown $user /home/$user -R
 chgrp $user /home/$user -R
