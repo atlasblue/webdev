8e0b28fd18bf339a487bf28b380838e69aab6d24
atlasblue <92083755+atlasblue@users.noreply.github.com>
2023-12-13 09:08:41 +0100

    Update setup2.sh

--- setup2.sh
+++ setup2.sh
@@ -46,19 +46,7 @@ systemctl stop firewalld
 systemctl disable firewalld
 setenforce 0
 
-echo "
-<p>&nbsp;</p>
-<p><span style="font-size:36px"><span style="font-family:Arial,Helvetica,sans-serif"><strong>Welcome to the Development environment</strong></span></span></p>
-<p>&nbsp;</p>
-<?php 
-$ip_server = $_SERVER['SERVER_ADDR']; 
-echo '<span style="font-size:36px"><span style="font-family:Arial,Helvetica,sans-serif"><strong>';
-echo 'Server IP Address is : $ip_server'; 
-?> 
-<p><img src="dev.png" /></p>
-" > $serverRoot/$router
-
-
+wget https://raw.githubusercontent.com/atlasblue/webdev/master/index.php -P $serverRoot
 wget https://raw.githubusercontent.com/atlasblue/webdev/master/dev.png -P $serverRoot
 chown $user /home/$user -R
 chgrp $user /home/$user -R
