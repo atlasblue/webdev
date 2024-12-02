5f44839bdd608e78aa4d219c140136da22cbb232
atlasblue <92083755+atlasblue@users.noreply.github.com>
2024-12-02 22:49:26 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -16,23 +16,3 @@ sudo systemctl restart php-fpm
 serverRoot="/usr/share/nginx/html"
 wget https://raw.githubusercontent.com/atlasblue/webdev/master/index.php -P $serverRoot
 wget https://raw.githubusercontent.com/atlasblue/webdev/master/dev.png -P $serverRoot
-rm -f /etc/nginx/conf.d/*
-router="index.php"
-domain="localhost"
-echo "
-server {
-    listen       80;
-    server_name  l localhost $domain www.$domain;
-    root $serverRoot;
-    index $router;
-    location / {
-        try_files \$uri \$uri/ /$router;
-    }
-    location ~ \.php$ {
-        fastcgi_index   $router;
-        fastcgi_pass    127.0.0.1:9000;
-        include         fastcgi_params;
-        fastcgi_param   SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
-    }
-}
-" > /etc/nginx/conf.d/server.conf
